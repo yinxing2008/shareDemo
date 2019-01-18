93d314927582cb67399d3e86242da961a7bc0567
jerry <309032663@qq.com>
2019-01-18 10:42:16 +0800

    将第三方key配置到strings文件中

--- app/src/main/java/com/example/test/sharedemo/App.java
+++ app/src/main/java/com/example/test/sharedemo/App.java
@@ -6,6 +6,7 @@ import com.umeng.libs.share.ShareTool;
 import com.umeng.libs.share.ShareType;
 import com.umeng.libs.share.config.QQConfig;
 import com.umeng.libs.share.config.WeiXinConfig;
+import com.umeng.soexample.R;
 
 
 public class App extends Application {
@@ -20,9 +21,9 @@ public class App extends Application {
      * 初始化分享组件
      */
     private void initShareComponents() {
-        ShareTool.addPlatformConfig(ShareType.QQ, new QQConfig("100424468","c7394704798a158208a74ab60104f0ba"));
-        ShareTool.addPlatformConfig(ShareType.WEIXIN, new WeiXinConfig("wxdc1e388c3822c80b","3baf1193c85774b3fd9d18447d76cab0"));
-        ShareTool.init(getApplicationContext(),"59892f08310c9307b60023d0");
+        ShareTool.addPlatformConfig(ShareType.QQ, new QQConfig(getString(R.string.qq_app_id),getString(R.string.qq_app_key)));
+        ShareTool.addPlatformConfig(ShareType.WEIXIN, new WeiXinConfig(getString(R.string.weixin_app_id),getString(R.string.weixin_app_signature)));
+        ShareTool.init(getApplicationContext(),getString(R.string.umeng_key));
     }
 
 }
